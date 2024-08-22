23651ac71d9ae012ee1c1451679191bfcc338576
João Paulo <108131013+JooJPaulo@users.noreply.github.com>
2024-08-22 20:57:36 -0300

    Exemplos Aula 02

--- Aula 02/ParOuImpar.java
+++ Aula 02/ParOuImpar.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ParOuImpar {
 	public static void main(String[] args) {
 		int num;
-		Scanner entrada = new Scanner(System.in);
+		try (Scanner entrada = new Scanner(System.in)) {
+			System.out.println("Digite um número: ");
+			num = entrada.nextInt();
+		}
 		
-		System.out.println("Digite um número: ");
-		num = entrada.nextInt();
-
 		if(num % 2 == 0) {
 			System.out.printf("O número %d é par", num);
 		} else {
