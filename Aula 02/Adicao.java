2fb337aae4ef913f3f5558e74fe8e2cae4ef5411
João Paulo <108131013+JooJPaulo@users.noreply.github.com>
2024-08-22 20:53:40 -0300

    Exemplos da aula 02

---
+++ Aula 02/Adicao.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Adicao {
+	public static void main(String[] args) {
+		int num1, num2, soma;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro número: ");
+		num1 = entrada.nextInt();
+
+		System.out.println("Digite o segundo número: ");
+		num2 = entrada.nextInt();
+
+		soma = num1 + num2;
+
+		System.out.printf("A soma de %d e %d é: %d", num1, num2, soma);
+	}
+}
---
+++ Aula 02/ParOuImpar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+	public static void main(String[] args) {
+		int num;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		num = entrada.nextInt();
+
+		if(num % 2 == 0) {
+			System.out.printf("O número %d é par", num);
+		} else {
+			System.out.printf("O número %d é ímpar", num);
+
+		}
+	}
+}
