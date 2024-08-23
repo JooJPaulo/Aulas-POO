1ad3df47f3a395fba652f404f59c094cc38078b0
João Paulo <108131013+JooJPaulo@users.noreply.github.com>
2024-08-22 21:25:43 -0300

    Código exercícios aula 02

---
+++ Aula 02/Exercicios.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicios {
+	public static void main(String[] args) {
+		try (/*Exercício 01*/
+		Scanner scanner = new Scanner(System.in)) {
+			int num1, num2;
+
+			System.out.println("Digite o primeiro número: ");
+			num1 = scanner.nextInt();
+
+			System.out.println("Digite o segundo número: ");
+			num2 = scanner.nextInt();
+
+			System.out.println("A soma dos dois números é: ", num1 + num2);
+			System.out.println("A diferença dos dois números é: ", num1 - num2);
+			System.out.println("o produto dos dois números é: ", num1 * num2);
+			System.out.println("A divisão dos dois números é: ", num1 / num2);
+		}
+
+	}
+}
